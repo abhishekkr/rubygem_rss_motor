40af9419d00bf683ea23e537e501830bf45472d6
AbhishekKr <abhikumar163@gmail.com>
2012-09-08 11:32:36 +0530

    fixed tests up

--- spec/rss-motor_spec.rb
+++ spec/rss-motor_spec.rb
@@ -1,25 +1,67 @@
 require 'spec_helper.rb'
 
 describe Rss::Motor do
-  before(:all) {
+
+  before(:all) do
+    @rss_item = <<-RSS
+        <items><item>
+        <title>item2</title>
+        <link>some url_link</link>
+        <description>Lorem ipsum dolor sit amet.</description>
+        <author>Doctor Who</author>
+        <comments>no comment</comments>
+        <media:content size='8Bit'/>
+        </item></items>
+      RSS
+    @response = [{
+        'title'       => 'item2',
+        'link'        => 'some url_link',
+        'guid'        => '',
+        'description' => 'Lorem ipsum dolor sit amet.',
+        'date'        => '',
+        'author'      => 'Doctor Who',
+        'enclosure'   => ''
+      }]
+
+    @for_node = 'comments'
+    @response_for_node = {'comments' => 'no comment'}
+    @for_node_attrib = {'media:content' => 'size'}
+    @response_for_node_attrib = {'media:content' => '8Bit'}
     @url = "http://example.com"
-  }
+  end
 
   describe "rss_items" do
-    before(:all) do
-      @response = {
-        'title'       => 'FooBar title',
-        'link'        => 'some url_link',
-        'description' => 'Lorem ipsum dolor sit amet.',
-        'author'      => 'Doctor Who'
-      }
-      Rss::WWW.stubs(:rss_channel).returns([""])
-      Rss::Proc.stubs(:rss_hashr).returns(@response)
+    before(:each) do
+      Rss::WWW.stubs(:rss_channel).returns([@rss_item])
     end
 
-    it "should return Array of Hashes" do
+    it "should return Array of Hashes for default-nodes" do
       data = Rss::Motor.rss_items(@url)
       data.should eq(@response)
+    end
+
+    it "should return Array of Hashes for default-nodes and extra argument node" do
+      data = Rss::Motor.rss_items(@url, @for_node)
+      new_response = @response[0].merge @response_for_node
+      data.should eq([new_response])
+    end
+
+    it "should return Array of Hashes for default-nodes and extra argument nodes array" do
+      data = Rss::Motor.rss_items(@url, [@for_node])
+      new_response = @response[0].merge @response_for_node
+      data.should eq([new_response])
+    end
+
+    it "should return Array of Hashes for default-nodes and extra argument node's attribute hash" do
+      data = Rss::Motor.rss_items(@url, [], @for_node_attrib)
+      new_response = @response[0].merge @response_for_node_attrib
+      data.should eq([new_response])
+    end
+
+    it "should return Array of Hashes for default-nodes and extra argument nodes array" do
+      data = Rss::Motor.rss_items(@url, ['comments'], @for_node_attrib)
+      new_response = @response[0].merge(@response_for_node).merge @response_for_node_attrib
+      data.should eq([new_response])
     end
   end
 
@@ -84,5 +126,4 @@ describe Rss::Motor do
       res.should eq(false)
     end
   end
-
 end
--- spec/rss-proc_spec.rb
+++ spec/rss-proc_spec.rb
@@ -6,4 +6,18 @@ describe Rss::Proc do
     response = Rss::Proc.rss_hashr(data)
     response.should eq([{"title" => "FooBar",  "link"=>"", "guid"=>"", "description"=>"", "date"=>"", "author"=>"", "enclosure"=>""}])
   end
+
+  it "should returned Hash with required Node Value" do
+    data          = '<item><title>FooBar</title><uuid>007</uuid></item>'
+    required_node = 'uuid'
+    response = Rss::Proc.rss_hashr(data, required_node)
+    response.should eq([{'title' => 'FooBar',  'link'=>'', 'guid'=>'', 'description'=>'', 'date'=>'', 'author'=>'', 'enclosure'=>'', 'uuid'=>'007'}])
+  end
+
+  it "should returned Hash with required Node Value's Attribute" do
+    data        = '<item><title>FooBar</title><uu id="007"/></item>'
+    node_attrib = {'uu' => 'id'}
+    response    = Rss::Proc.rss_hashr(data, [], node_attrib)
+    response.should eq([{'title' => 'FooBar',  'link'=>'', 'guid'=>'', 'description'=>'', 'date'=>'', 'author'=>'', 'enclosure'=>'', 'uu'=>'007'}])
+  end
 end
